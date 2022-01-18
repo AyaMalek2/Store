3d073ca69b683dcf9f44e5d79c45e87b962efedc
AyaMalek <aya.malek142@gmail.com>
2022-01-18 19:40:52 +0200

    add product component

---
+++ src/app/modules/Products/product-details/product-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../products.service';
+import { Product } from '../product.model';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product:Product = new Product;
+
+
+  constructor(private productService :ProductService,private activatedroute:ActivatedRoute) { }
+
+
+  ngOnInit(): void {
+  }
+
+}
